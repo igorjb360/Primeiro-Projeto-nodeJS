e56b89febc8ccea9348f32c05398b837baff24aa
Igor José Bourscheid <igorjb360@gmail.com>
2021-09-22 17:41:55 -0300

    aprendendo a mexer com node

--- src/view/route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-
-const route = express.Router()   
-
-route.get('/',(req, res) =>  res.render("index")) //codigo para rodar o site no navegador
-
-module.exports = route
