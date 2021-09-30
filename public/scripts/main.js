725f4f9cccad9af024fd8af621a112b429b58d5e
Igor José Bourscheid <igorjb360@gmail.com>
2021-09-30 15:47:49 -0300

    ultima aula que fiz do discover o cod Incompleto

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -16,10 +16,7 @@ const checkButtons = document.querySelectorAll(".actions a.check")
 // aqui estou checando o bottao 
 checkButtons.forEach(button => {
     // add a escuta quando clicar vai abrir a modal-wrapper
-    button.addEventListener ("click", event => {
-        //abrir modal
-        modal.open()
-    })
+    button.addEventListener ("click", handleClick)
 })
 
 // aqui vou fazer a logica do delete para abrir a modalwrapper usa All para pegar todas as variaveis
@@ -27,10 +24,28 @@ const deletebutton = document.querySelectorAll(".actions a.delete")
 
 deletebutton.forEach(button => {
     // add a escuta quando clicar vai abrir a modal-wrapper
-    button.addEventListener ("click", event => {
-        //abrir modal
-        modal.open()
-    })
+    button.addEventListener ("click", (event) => handleClick(event, false))
 })
+
+function handleClick(event, check = true){
+    // aqui vai parar de alterar nossa url
+    event.preventDefault()
+    //aqui estou atribuindo essa variavel para siplificar o uso do text e do check
+    const text = check ? "Marcar como lida" : "Excluir"
+    const slug = check ? "check" : "delete"
+    const roomId = document.querySelector("#room-id").dataset.id
+    const questionId = event.target.dataset.id
+
+    const form = document.querySelector(".modal form")
+    form.setAttribute("action", `/room/${roomId}/${questionId}/${slug}`)
+   
+    modalTitle.innerHTML = `${text} Essa pergunta`
+    modalDescription.innerHTML = `tem certeza que deseja ${text.toLowerCase()} essa pergunta?` 
+    modalButton.innerHTML = `Sim, ${text.toLowerCase()} `
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+
+    //abrir modal
+    modal.open()
+}
 
 
--- src/route.js
+++ src/route.js
@@ -1,9 +1,16 @@
-const express = require('express')
+const express = require('express') //erro que preciso lembrar eu nao tinha ideia do que era eu nao tinha atribuido a variavel controller
+const QuestionController = require('./controllers/QuestionController')
 
 const route = express.Router()
 
-route.get('/', (req, res) => res.render("index"))
+route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))   
+route.get('/creat-pass' , (req, res) => res.render("index", {page: 'creat-pass'}))
+
 route.get('/room' , (req, res) => res.render("room"))
-route.get('/creat-pass' , (req, res) => res.render("creat-pass"))
+
+// preciso ver melhor o que é req e res ??
+
+//formato que o formulario de dentro da modal tem que passar a informação
+route.post('/room/:room/:question/:action', QuestionController.index)
 
 module.exports = route;
